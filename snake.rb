485ea6da630dd457d56bb3d3f810441bb79ed94d
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-03-21 11:31:11 -0400

    removed version number in exchange for tar containing version number

--- snake.rb
+++ snake.rb
@@ -2,7 +2,6 @@ class Snake < Formula
   desc "simple snake game. Written in pure java"
   homepage "http://silo.cs.indiana.edu:32903/snake/snakehome.html"
   url "http://silo.cs.indiana.edu:32903/snake/snake.tar.gz"
-  version "1.0.0"
   sha256 "8da49ab43bfa442fc21890c49cc317e7393aa2e82112b8cd55b19f2373e795a6"
   depends_on "ant" => :build
   depends_on :java
