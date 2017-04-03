a96fc573abc55ed35c194ed76ad9369288c656a2
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-04-02 20:01:15 -0400

    removed line printing debug message

--- snake.rb
+++ snake.rb
@@ -2,7 +2,7 @@ class Snake < Formula
   desc "simple snake game. Written in pure java"
   homepage "http://silo.cs.indiana.edu:32903/snake/snakehome.html"
   url "http://silo.cs.indiana.edu:32903/snake/snake-1.0.2.tar.gz"
-  sha256 "fcfb8d20ff34717302c473ac4cc3f7e2a90cecd9ac0019fbb23cb77551a27b0b"
+  sha256 "cd977b155bed5ef5a9dbe0aa86ac45afb21c8d959ce67f20f068cc68f8027aca"
   depends_on "ant" => :build
   depends_on :java
   def install
