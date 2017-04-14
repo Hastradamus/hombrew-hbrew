3beeec90fe690a761ea4a77474f0bd3523537361
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-04-13 20:03:07 -0400

    updated sha to reflect changes

--- snake.rb
+++ snake.rb
@@ -2,7 +2,7 @@ class Snake < Formula
   desc "simple snake game. Written in pure java"
   homepage "http://silo.cs.indiana.edu:32903/snake/snakehome.html"
   url "http://silo.cs.indiana.edu:32903/snake/snake-1.0.3.tar.gz"
-  sha256 "bd4d42e841c5b3e4989562f1be2376226f1c80bae30f57a6cb545c93a2e507c0"
+  sha256 "bcbc3f456b6f3cb4f80e93b46b7d9615225e346c3b81d257e298f249aaf14275"
   depends_on :java
   def install
       system "ruby", "build.rb", "compile", "jar"
