498e92bedcba9cf35c41d740f747268d6fea51dd
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-04-14 01:07:37 -0400

    merge repo containing snake .java files and repo containing homebrew .rb file

--- snake.rb
+++ snake.rb
@@ -2,11 +2,11 @@ class Snake < Formula
   desc "simple snake game. Written in pure java"
   homepage "http://silo.cs.indiana.edu:32903/snake/snakehome.html"
   url "http://silo.cs.indiana.edu:32903/snake/snake-1.0.3.tar.gz"
-  sha256 "bd4d42e841c5b3e4989562f1be2376226f1c80bae30f57a6cb545c93a2e507c0"
+  sha256 "36c94acd19bf8bcfb0301e9fd9459f61a36b6213679dbe6bd103fc6816aff3b8"
   depends_on :java
   def install
       system "ruby", "build.rb", "compile", "jar"
-    mv "snake.jar", "#{prefix}/"
+    mv "*.jar", "#{prefix}/"
     bin.install "snake"
   end
   test do
