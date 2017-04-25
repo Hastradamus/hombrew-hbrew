6b671827165e9547b676f0bec9e15d09c2a5a338
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-04-25 10:18:46 -0400

    corrected version number

--- snake.rb
+++ snake.rb
@@ -1,7 +1,7 @@
 class Snake < Formula
   desc "simple snake game. Written in pure java"
   homepage "http://silo.cs.indiana.edu:32903/snake/snakehome.html"
-  url "http://silo.cs.indiana.edu:32903/snake/snake-1.0.3.tar.gz"
+  url "http://silo.cs.indiana.edu:32903/snake/snake-1.0.4.tar.gz"
   sha256 "8e35203f41bea7476334fb761e5a616c5a58f3472ebac706ffd4a0abf454cd87"
   def install
     system "ruby", "build.rb", "osec"
