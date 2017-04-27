a8ee913c0ebb6ab6e09ffb2498c21efaa15f9332
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-04-27 10:00:35 -0400

    changed brew formula

--- snake.rb
+++ snake.rb
@@ -2,7 +2,7 @@ class Snake < Formula
   desc "simple snake game. Written in pure java"
   homepage "http://silo.cs.indiana.edu:32903/snake/snakehome.html"
   url "http://silo.cs.indiana.edu:32903/snake/snake-1.0.4.tar.gz"
-  sha256 "f7f9160643f6b67d1b9442ac0afd87dc600568644ff18ba13f05f7bf35fbedc6"
+  sha256 "3d6ee2d67a489993f9b04730017f0cdba2efa8b7bc71cc54924ea695c9f27378"
 
   jre = "jre1.8.0_131.jre"
   jas = "jasypt-1.9.2.jar"
