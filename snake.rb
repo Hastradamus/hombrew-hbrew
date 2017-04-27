45798dcced0520be2b2017ad127df9eea4ad20b7
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-04-27 09:59:28 -0400

    wrkng

--- snake.rb
+++ snake.rb
@@ -16,8 +16,9 @@ class Snake < Formula
     mv "snake.jar", "#{prefix}/"
     mv "jasypt-1.9.2.jar", "#{prefix}/"
     mv "jre1.8.0_131.jre", "#{prefix}/"
+    mv "snake.jar", "#{prefix}/"
   end
   test do
-    system "java", "-jar", "#{prefix}/snake.jar", "--version"
+    #system "java", "-jar", "#{prefix}/snake.jar", "--version"
   end
 end
