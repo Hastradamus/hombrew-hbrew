790d9426543eef583e177248b36fda8e4dc59a94
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-12-19 17:34:50 -0500

    changed brew formula

--- snake.rb
+++ snake.rb
@@ -1,4 +1,4 @@
-#class Snake < Formula
+class Snake < Cask
 cask 'Simple Snake' do
     sha256 "9886f049310dbcd6516b3bda081a082b7c86880251972f32a6b190bd3384257b"
     url "https://github.com/hasahmed/homebrew-hbrew/releases/download/2.0.0/simplesnake.tar.gz"
