357157750be47c2c69badc6c7c1b17161b07893b
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-12-19 16:56:38 -0500

    modified snake.rb

--- snake.rb
+++ snake.rb
@@ -1,7 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class HomebrewHbrew < Formula
   desc "A simple snake game written in pure Java"
   homepage "http://pages.iu.edu/~hasahmed/snake/"
@@ -12,15 +8,6 @@ class HomebrewHbrew < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test homebrew-hbrew`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "true"
   end
 end
