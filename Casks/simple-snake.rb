41693075f472f87eda2073f26056f2213b3d069a
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-12-19 18:01:31 -0500

    changed brew formula

--- Casks/simple-snake.rb
+++ Casks/simple-snake.rb
@@ -1,5 +1,6 @@
 cask 'simple-snake' do
   sha256 "9886f049310dbcd6516b3bda081a082b7c86880251972f32a6b190bd3384257b"
+  # https://github.com/hasahmed/ was verified as official when first introduced to the cask
   url "https://github.com/hasahmed/homebrew-hbrew/releases/download/2.0.0/simplesnake.tar.gz"
   name 'Simple Snake'
   homepage "http://pages.iu.edu/~hasahmed/snake/"
