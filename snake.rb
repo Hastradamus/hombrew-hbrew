21640c7b810f11f3534b1812aee554c16db2b58a
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-12-19 16:54:26 -0500

    changed brew formula

--- snake.rb
+++ snake.rb
@@ -1,23 +1,26 @@
-class Snake < Formula
-  desc "simple snake game. Written in pure java"
-  homepage "http://silo.cs.indiana.edu:32903/snake/snakehome.html"
-  url "http://silo.cs.indiana.edu:32903/snake/snake-1.0.4.tar.gz"
-  sha256 "3d6ee2d67a489993f9b04730017f0cdba2efa8b7bc71cc54924ea695c9f27378"
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-  jre = "jre1.8.0_131.jre"
-  jas = "jasypt-1.9.2.jar"
-  snakejar = "snake.jar"
-
+class HomebrewHbrew < Formula
+  desc "A simple snake game written in pure Java"
+  homepage "http://pages.iu.edu/~hasahmed/snake/"
+  url "https://github.com/hasahmed/homebrew-hbrew/releases/download/2.0.0/simplesnake.tar.gz"
+  sha256 "9886f049310dbcd6516b3bda081a082b7c86880251972f32a6b190bd3384257b"
   def install
-    ohai "#{prefix}"
-    bin.install "snake"
-    system "ruby", "build.rb", "osec"
-    system "ruby", "build.rb", "extractjre"
-    mv "snake.jar", "#{prefix}/"
-    mv "jasypt-1.9.2.jar", "#{prefix}/"
-    mv "jre1.8.0_131.jre", "#{prefix}/"
+    mv "Simple Snake.app", "/Applications"
   end
+
   test do
-    #system "java", "-jar", "#{prefix}/snake.jar", "--version"
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-hbrew`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
   end
 end
