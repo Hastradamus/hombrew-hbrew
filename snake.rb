1bee4a9cdbc05a3c9db11026da3a836bd9cb5452
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-12-19 17:32:32 -0500

    changed brew formula

--- snake.rb
+++ snake.rb
@@ -1,11 +1,15 @@
 class Snake < Formula
-  desc "A simple snake game written in pure Java"
-  homepage "http://pages.iu.edu/~hasahmed/snake/"
-  url "https://github.com/hasahmed/homebrew-hbrew/releases/download/2.0.0/simplesnake.tar.gz"
-  sha256 "9886f049310dbcd6516b3bda081a082b7c86880251972f32a6b190bd3384257b"
-  def install
-    system "cd", "../"
-    mv "Simple Snake.app", "/Applications"
+  cask 'Simple Snake' do
+    sha256 "9886f049310dbcd6516b3bda081a082b7c86880251972f32a6b190bd3384257b"
+    url "https://github.com/hasahmed/homebrew-hbrew/releases/download/2.0.0/simplesnake.tar.gz"
+    name 'Simple Snake'
+    #desc "A simple snake game written in pure Java"
+    homepage "http://pages.iu.edu/~hasahmed/snake/"
+    app 'Simple Snake.app'
+  #def install
+    #system "cd", "../"
+    #mv "Simple Snake.app", "~/Applications"
+  #end
   end
 
   test do
